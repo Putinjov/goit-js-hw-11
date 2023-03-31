1d442869c23710c705ea01b1cf7136cef8040839
Putinjov <putinjov@gmail.com>
2023-03-31 21:49:46 +0300

    pull

---
+++ src/getImage.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_KEY = '34785269-2da0cadfc3fd212a10b88586f';
+const BASE_URL = 'https://pixabay.com/api/';
+
+async function fetchImages(query, page = 1) {
+  const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
+  return response.data;
+}
+
+export default { fetchImages };
---
+++ src/index.js
@@ -0,0 +1,100 @@
+import Notiflix from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import fetchImages from './getImage';
+
+const form = document.querySelector("input[name=searchQuery]");
+const gallery = document.querySelector(".gallery");
+
+let pageNumber = 1;
+let currentQuery = "";
+
+const lightbox = new SimpleLightbox(".gallery a", {});
+
+form.addEventListener("submit", onSearch);
+
+function onSearch(event) {
+  event.preventDefault();
+  const searchQuery = form.dataset.value.trim();
+  console.log(searchQuery);
+  if (searchQuery === "") {
+    return Notiflix.Notify.failure("Input query!");
+  }
+
+  if (searchQuery !== currentQuery) {
+    clearGallery();
+    pageNumber = 1;
+  }
+
+  currentQuery = searchQuery;
+
+  fetchImages(currentQuery, pageNumber)
+    .then((response) => {
+      const hits = response.data.hits;
+      const totalHits = response.data.totalHits;
+      if (hits.length === 0) {
+        Notiflix.Notify.failure(
+          "Sorry, there are no images matching your search query. Please try again."
+        );
+        return;
+      }
+
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+
+      const imagesMarkup = createImagesMarkup(hits);
+      addImagesToGallery(imagesMarkup);
+      pageNumber +=1;
+    })
+    .catch((error) => {
+      console.log(error);
+      Notiflix.Notify.failure("Oops, something went wrong...");
+    });
+}
+
+function createImagesMarkup(images) {
+  return images
+    .map(({ webformatURL, largeImageURL, likes, views, comments, downloads, tags }) => {
+      return `
+        <div class="photo-card">
+          <a href="${webformatURL}">
+            <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+          </a>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b> ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${downloads}
+            </p>
+          </div>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+function addImagesToGallery(markup) {
+  gallery.insertAdjacentHTML("beforeend", markup);
+  lightbox.refresh();
+}
+
+function clearGallery() {
+  gallery.innerHTML = "";
+}
+
+window.addEventListener("scroll", onScroll);
+
+function onScroll() {
+const { height: cardHeight } =gallery
+  .firstElementChild.getBoundingClientRect();
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+}
